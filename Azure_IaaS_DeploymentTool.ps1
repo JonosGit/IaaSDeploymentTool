29f5d5e1543487a858d176e8e741cd4358b6c458
John Lewis <sassy.seattle.engineer@gmail.com>
2016-06-27 12:04:46 -0700

    merged versions

--- Azure_IaaS_DeploymentTool.ps1
+++ Azure_IaaS_DeploymentTool.ps1
@@ -1,80 +1,154 @@
 ﻿<# 
 .SYNOPSIS 
-This script provides the following functionality for deploying Iaas environments in Azure.
-Market Images supported: Redhat 6.7, PFSense 2.5, Windows 2008 R2, Windows 2012 R2, Ubuntu, CentOs, SUSE and Chef 12 Server
+This script provides the following functionality for deploying IaaS environments in Azure. The script will deploy VNET in addition to numerour Market Place VMs or make use of an existing VNET.
+Market Images supported: Redhat 6.7, PFSense 2.5, Windows 2008 R2, Windows 2012 R2, Ubuntu 14.04, CentOs 7.2, SUSE, SQL 2016 (on W2K12R2), R Server on Windows and Chef Server v12
 
-Prior to running this script make sure the environment variables have been configured for Subscription ID, TenantID, ResourceGroup and VNETName
-Update the vmcount variable to increment the Deployed VM, i.e. 001 will result in a VM Deployed named AIP001.
-To specify the local adminsitrator and Password Update the locadmin and locpassword
-The Public IP Address of the VM will be shown when the script completes and can be used access the server.
+.PARAMETERS
+VMName is a required parameter at runtime. All other parameters are optional at runtime.
 
-By default a new VNET and dual homed PFSense Server are deployed to a new or existing Resource Group in Azure.
+
 To deploy a VM to an existing VNET set the $NewVnet parameter to false. ** Update the $VNETResourceGroupName variable before running the script.
 To deploy a new VNET with multiple subnets set the $NewVnet flag to true. ** The New VNET Will be deployed to $vNetResourceGroupName.
+To deploy a specific market image enter one of the following names for $vmmMarketImage: Redhat PFSecure W2k12r2 w2k8r2 centos ubuntu chef SUSE SQL RSERVER
 
-To deploy a specific market image enter one of the following names for $vmmMarketImage: Redhat PFSecure W2k12r2 w2k8r2 centos ubuntu chef SUSE (default is pfsense)
+The Public IP Address of the VM will be shown when the script completes and can be used access the server.
+
+.EXAMPLES
+Deployment runtime positional parameters examples:
+.\Azure_infra_arm_automater.ps1myserver RedHat myresgroup myvnet -NewVNET True -NSGEnabled True
+.\Azure_infra_arm_automater.ps1myserver2 RedHat myresgroup myvnet
+.\Azure_infra_arm_automater.ps1myserver3 Suse myresgroup myvnet
+.\Azure_infra_arm_automater.ps1myserver4 w2k12 myresgroup myvnet
+.\Azure_infra_arm_automater.ps1myserver5 rserver myresgroup myvnet
+.\Azure_infra_arm_automater.ps1myserver5 sql myresgroup myvnet
+
+Runtime named parameters examples:
+Deploy SQL Server to existing VNET in existing resource group: .\Azure_infra_arm_automater.ps1-VName sqlserver1 -VMMarketImage SQL
+Deploy PFSense Server to a existing VNET in existing resource group: .\Azure_infra_arm_automater.ps1-VName pfserver1 -VMMarketImage Pfsense
+Deploy PFSense Server to a new VNET in new resource group with a Network Security Group attached: .\Azure_infra_arm_automater.ps1-VName pfserver1 -VMMarketImage Pfsense -NewVNET True -VNETName NewVNET -ResourceGroupName INFRA_RG -NSGEnabled True 
+Deploy Windows 2012 R2 Server to a new VNET in new resource group: .\Azure_infra_arm_automater.ps1-VName winserver1 -VMMarketImage w2k12r2
 #> 
 
+Param( 
+ [Parameter(Mandatory=$False,ValueFromPipeline=$True,Position=1)]
+ [string]
+ $vmMarketImage = "PFSense",
 
+ [Parameter(Mandatory=$False,ValueFromPipeline=$True,Position=5)]
+ [string]
+ $NewVnet = "False",
+
+ [Parameter(Mandatory=$True,ValueFromPipeline=$True,Position=0)]
+ [string]
+ $VMName = "server0001",
+
+ [Parameter(Mandatory=$False,ValueFromPipeline=$True,Position=2)]
+ [string]
+ $ResourceGroupName = "RESGRP",
+
+ [Parameter(Mandatory=$False,ValueFromPipeline=$True,Position=4)]
+ [string]
+ $vNetResourceGroupName = $ResourceGroupName,
+
+ [Parameter(Mandatory=$False,ValueFromPipeline=$True,Position=3)]
+ [string]
+ $VNetName = "VNET",
+
+ [Parameter(Mandatory=$False,ValueFromPipeline=$True)]
+ [string]
+ $VMSize = "Standard_A3",
+
+ [Parameter(Mandatory=$False,ValueFromPipeline=$True)]
+ [string]
+ $locadmin = 'localadmin',
+
+ [Parameter(Mandatory=$False)]
+ [string]
+ $locpassword = 'PassW0rd!@1',
+
+ [Parameter(Mandatory=$False,ValueFromPipeline=$True,Position=6)]
+ [string]
+ $NSGEnabled = "False",
+
+ [Parameter(Mandatory=$False,ValueFromPipeline=$True)]
+ [string]
+ $Location = "WestUs",
+
+ [Parameter(Mandatory=$False,ValueFromPipeline=$True)]
+ [string]
+ $SubscriptionID = '',
+
+ [Parameter(Mandatory=$False,ValueFromPipeline=$True)]
+ [string]
+ $TenantID = '',
+
+ [Parameter(Mandatory=$False,ValueFromPipeline=$True)]
+ [string]
+ $StorageName = $VMName + "str",
+
+ [Parameter(Mandatory=$False,ValueFromPipeline=$True)]
+ [string]
+ $StorageType = "Standard_GRS",
+
+ [Parameter(Mandatory=$False,ValueFromPipeline=$True)]
+ [string]
+ $InterfaceName1 = $VMName + "nic1",
+
+ [Parameter(Mandatory=$False,ValueFromPipeline=$True)]
+ [string]
+ $InterfaceName2 = $VMName + "nic2",
+
+ [Parameter(Mandatory=$False,ValueFromPipeline=$True)]
+ [string]
+ $NSGName = "AIPNSG"
+
+)
 
 ## Global
-$NewVnet = "True"
-$vmMarketImage = "pfsense"
-$vmcount = "0001"
-$vmtype = "azvm"
-$VMName = $vmtype + $vmcount
-$ResourceGroupName = "RESOURCEGROUP"
-$vNetResourceGroupName = $ResourceGroupName
-$Location = "WestUs"
-$SubscriptionID=''
-$TenantID=''
-$StorageName = $VMName + "str"
-$StorageType = "Standard_GRS"
-$InterfaceName1 = $VMName + "nic1"
-$InterfaceName2 = $VMName + "nic2"
-$VNetName = "VNET"
-$VMSize = "Standard_A3"
-$locadmin = 'localadmin'
-$locpassword = 'PassW0rd!@1'
 $SecureLocPassword=Convertto-SecureString $locpassword –asplaintext -force
 $Credential1 = New-Object System.Management.Automation.PSCredential ($locadmin,$SecureLocPassword)
-
 
 # Login-AzureRmAccount
 Set-AzureRmContext -tenantid $TenantID -subscriptionid $SubscriptionID
 
 # Resource Group
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location -force
+New-AzureRmResourceGroup -Name $vNetResourceGroupName -Location $Location -force
 
 If($NewVnet -eq "True")
 {
-
 ## Create Virtual Network
 Write-Host "Network Deployment in Progress"
-$NewVirtualNetwork = @{
-    ResourceGroupName = $vNetResourceGroupName;
-    Name = $VNetName;
-    Location = 'West US';
-    AddressPrefix = '10.51.0.0/21';
-    Subnet = $SubnetList;
-    }
-New-AzureRmVirtualNetwork @NewVirtualNetwork;
-$SubnetList = @();
-$SubnetList += New-AzureRmVirtualNetworkSubnetConfig -Name enablement -AddressPrefix 10.51.0.0/24;
-$SubnetList += New-AzureRmVirtualNetworkSubnetConfig -Name public -AddressPrefix 10.51.1.0/24;
-$SubnetList += New-AzureRmVirtualNetworkSubnetConfig -Name ingest -AddressPrefix 10.51.2.0/24;
-$SubnetList += New-AzureRmVirtualNetworkSubnetConfig -Name data -AddressPrefix 10.51.3.0/24;
-$SubnetList += New-AzureRmVirtualNetworkSubnetConfig -Name monitor -AddressPrefix 10.51.4.0/24;
-$SubnetList += New-AzureRmVirtualNetworkSubnetConfig -Name analytics -AddressPrefix 10.51.5.0/24;
-$SubnetList += New-AzureRmVirtualNetworkSubnetConfig -Name backup -AddressPrefix 10.51.6.0/24;
-$SubnetList += New-AzureRmVirtualNetworkSubnetConfig -Name management -AddressPrefix 10.51.7.0/24;
+$subnet1 = New-AzureRmVirtualNetworkSubnetConfig -AddressPrefix 10.51.0.0/24 -Name enablement
+$subnet2 = New-AzureRmVirtualNetworkSubnetConfig -AddressPrefix 10.51.1.0/24 -Name Public
+$subnet3 = New-AzureRmVirtualNetworkSubnetConfig -AddressPrefix 10.51.2.0/24 -Name ingest
+$subnet4 = New-AzureRmVirtualNetworkSubnetConfig -AddressPrefix 10.51.3.0/24 -Name data
+$subnet5 = New-AzureRmVirtualNetworkSubnetConfig -AddressPrefix 10.51.4.0/24 -Name monitoring
+$subnet6 = New-AzureRmVirtualNetworkSubnetConfig -AddressPrefix 10.51.5.0/24 -Name analytics
+$subnet7 = New-AzureRmVirtualNetworkSubnetConfig -AddressPrefix 10.51.6.0/24 -Name backup
+$subnet8 = New-AzureRmVirtualNetworkSubnetConfig -AddressPrefix 10.51.7.0/24 -Name management
+New-AzureRmVirtualNetwork -Location WestUS -Name $VNetName -ResourceGroupName $vNetResourceGroupName -AddressPrefix '10.51.0.0/21' -Subnet $subnet1,$subnet2,$subnet3,$subnet4, $subnet5, $subnet6, $subnet7 -Force;
+
+Get-AzureRmVirtualNetwork -Name VNET -ResourceGroupName MyNewRes7 | Get-AzureRmVirtualNetworkSubnetConfig | ft "addressprefix"
+
+Write-Host "Completed deployment of new VNET"
+}
+If($NSGEnabled -eq "True")
+{
+Write-Host "Network Security Group Deployment in Progress"
+$httprule = New-AzureRmNetworkSecurityRuleConfig -Name "FrontEnd_HTTP" -Description "HTTP Exception for Web frontends" -Protocol Tcp -SourcePortRange "80" -DestinationPortRange "80" -SourceAddressPrefix "*" -DestinationAddressPrefix "10.51.0.0/21" -Access Allow -Direction Inbound -Priority 200
+$httpsrule = New-AzureRmNetworkSecurityRuleConfig -Name "FrontEnd_HTTPS" -Description "HTTPS Exception for Web frontends" -Protocol Tcp -SourcePortRange "443" -DestinationPortRange "443" -SourceAddressPrefix "*" -DestinationAddressPrefix "10.51.0.0/21" -Access Allow -Direction Inbound -Priority 201
+$sshrule = New-AzureRmNetworkSecurityRuleConfig -Name "FrontEnd_SSH" -Description "SSH Exception for Web frontends" -Protocol Tcp -SourcePortRange "22" -DestinationPortRange "22" -SourceAddressPrefix "*" -DestinationAddressPrefix "10.51.0.0/21" -Access Allow -Direction Inbound ` -Priority 203
+$nsg = New-AzureRmNetworkSecurityGroup -ResourceGroupName $vNetResourceGroupName -Location "West US" -Name $NSGName -SecurityRules $httprule,$httpsrule, $sshrule
+Get-AzureRmNetworkSecurityGroup -Name $NSGName -ResourceGroupName $vNetResourceGroupName
 }
 
-# Storage
-$StorageAccount = New-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageName -Type $StorageType -Location $Location
 
 
 ## Add Non Image Specific objects
+# Storage
+$StorageAccount = New-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageName -Type $StorageType -Location $Location
+
 
 switch -Wildcard ($vmMarketImage)
     {
@@ -94,7 +168,7 @@ $OSDiskName = $VMName + "OSDisk"
 $OSDiskUri = $StorageAccount.PrimaryEndpoints.Blob.ToString() + "vhds/" + $OSDiskName + ".vhd"
 $VirtualMachine = Set-AzureRmVMOSDisk -VM $VirtualMachine -Name $OSDiskName -VhdUri $OSDiskUri -CreateOption "FromImage" -Caching $osDiskCaching
 }
-        "*re*" {
+        "*red*" {
 Write-Host "Red Hat Deployment in Progress"
 $PIp = New-AzureRmPublicIpAddress -Name $InterfaceName1 -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod "Dynamic"
 $VNet = Get-AzureRMVirtualNetwork -Name $VNetName -ResourceGroupName $vNetResourceGroupName | Set-AzureRmVirtualNetwork
@@ -115,6 +189,33 @@ $Interface1 = New-AzureRmNetworkInterface -Name $InterfaceName1 -ResourceGroupNa
 $VirtualMachine = New-AzureRmVMConfig -VMName $VMName -VMSize $VMSize
 $VirtualMachine = Set-AzureRmVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $VMName -Credential $Credential1 -ProvisionVMAgent -EnableAutoUpdate
 $VirtualMachine = Set-AzureRmVMSourceImage -VM $VirtualMachine -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer" -Skus "2012-R2-Datacenter" -Version "latest"
+$VirtualMachine = Add-AzureRmVMNetworkInterface -VM $VirtualMachine -Id $Interface1.Id -Primary
+$OSDiskName = $VMName + "OSDisk"
+$OSDiskUri = $StorageAccount.PrimaryEndpoints.Blob.ToString() + "vhds/" + $OSDiskName + ".vhd"
+$VirtualMachine = Set-AzureRmVMOSDisk -VM $VirtualMachine -Name $OSDiskName -VhdUri $OSDiskUri -CreateOption "FromImage" -Caching $osDiskCaching
+}
+        "*sql*" {
+Write-Host "SQL Deployment in Progress"
+$PIp = New-AzureRmPublicIpAddress -Name $InterfaceName1 -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod "Dynamic"
+$VNet = Get-AzureRMVirtualNetwork -Name $VNetName -ResourceGroupName $vNetResourceGroupName | Set-AzureRmVirtualNetwork
+$Interface1 = New-AzureRmNetworkInterface -Name $InterfaceName1 -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $VNet.Subnets[4].Id -PublicIpAddressId $PIp.Id
+$VirtualMachine = New-AzureRmVMConfig -VMName $VMName -VMSize $VMSize
+$VirtualMachine = Set-AzureRmVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $VMName -Credential $Credential1 -ProvisionVMAgent -EnableAutoUpdate
+$VirtualMachine = Set-AzureRmVMSourceImage -VM $VirtualMachine -PublisherName "MicrosoftSQLServer" -Offer "SQL2016-WS2012R2" -Skus "Enterprise" -Version "latest"
+$VirtualMachine = Add-AzureRmVMNetworkInterface -VM $VirtualMachine -Id $Interface1.Id -Primary
+$OSDiskName = $VMName + "OSDisk"
+$OSDiskUri = $StorageAccount.PrimaryEndpoints.Blob.ToString() + "vhds/" + $OSDiskName + ".vhd"
+$VirtualMachine = Set-AzureRmVMOSDisk -VM $VirtualMachine -Name $OSDiskName -VhdUri $OSDiskUri -CreateOption "FromImage" -Caching $osDiskCaching
+}
+        "*rserver*" {
+Write-Host "R Server Deployment in Progress"
+$PIp = New-AzureRmPublicIpAddress -Name $InterfaceName1 -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod "Dynamic"
+$VNet = Get-AzureRMVirtualNetwork -Name $VNetName -ResourceGroupName $vNetResourceGroupName | Set-AzureRmVirtualNetwork
+$Interface1 = New-AzureRmNetworkInterface -Name $InterfaceName1 -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $VNet.Subnets[6].Id -PublicIpAddressId $PIp.Id
+$VirtualMachine = New-AzureRmVMConfig -VMName $VMName -VMSize $VMSize
+$VirtualMachine = Set-AzureRmVMPlan -VM $VirtualMachine -Name msr80-win2012r2 -Publisher microsoft-r-products -Product microsoft-r-server
+$VirtualMachine = Set-AzureRmVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $VMName -Credential $Credential1 -ProvisionVMAgent -EnableAutoUpdate
+$VirtualMachine = Set-AzureRmVMSourceImage -VM $VirtualMachine -PublisherName microsoft-r-products -Offer microsoft-r-server -Skus msr80-win2012r2 -Version "latest"
 $VirtualMachine = Add-AzureRmVMNetworkInterface -VM $VirtualMachine -Id $Interface1.Id -Primary
 $OSDiskName = $VMName + "OSDisk"
 $OSDiskUri = $StorageAccount.PrimaryEndpoints.Blob.ToString() + "vhds/" + $OSDiskName + ".vhd"
@@ -190,6 +291,8 @@ $VirtualMachine = Set-AzureRmVMOSDisk -VM $VirtualMachine -Name $OSDiskName -Vhd
     }
 
 ## Create the VM in Azure
+Write-Host "Starting Azure VM Creation"
 New-AzureRmVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $VirtualMachine -Verbose 
 Write-Host "Deployment Completed"
 Get-AzureRmPublicIpAddress -Name $InterfaceName1 -ResourceGroupName $ResourceGroupName | ft "IpAddress"
+Write-Host "Remote Access availble via IP Address above"
--- README.md
+++ README.md
@@ -1,18 +1,27 @@
-Deployment Instructions for Azure IaaS Deployment Tool
+.SYNOPSIS 
+This script provides the following functionality for deploying IaaS environments in Azure. The script will deploy VNET in addition to numerour Market Place VMs or make use of an existing VNET.
+Market Images supported: Redhat 6.7, PFSense 2.5, Windows 2008 R2, Windows 2012 R2, Ubuntu 14.04, CentOs 7.2, SUSE, SQL 2016 (on W2K12R2), R Server on Windows and Chef Server v12
 
-By default this script will deploy a new VNET with 7 subnets. It will then provision a PfSecure dual homed VPN to the edge subnet.
+.PARAMETERS
+VMName is a required parameter at runtime. All other parameters are optional at runtime.
 
-This script also supports deploying the following images ad-hoc Windows 2012 R2, Windows 2008 R2, Ubuntu, Redhat, Suse, Chef and Centos Store images on demand by changing one value in the script.
-
-Prior to running this script make sure the environment variables have been configured for Subscription ID, TenantID, ResourceGroup and VNETName
-Update the vmcount variable to increment the Deployed VM, i.e. 001 will result in a VM Deployed named AIP001.
-To specify the local adminsitrator and Password Update the locadmin and locpassword
-The Public IP Address of the VM will be shown when the script completes and can be used access the server.
-
-By default a new VNET and dual homed PFSense Server are deployed to a new or existing Resource Group in Azure.
 To deploy a VM to an existing VNET set the $NewVnet parameter to false. ** Update the $VNETResourceGroupName variable before running the script.
 To deploy a new VNET with multiple subnets set the $NewVnet flag to true. ** The New VNET Will be deployed to $vNetResourceGroupName.
+To deploy a specific market image enter one of the following names for $vmmMarketImage: Redhat PFSecure W2k12r2 w2k8r2 centos ubuntu chef SUSE SQL RSERVER
 
-To deploy a specific market image enter one of the following names for $vmmMarketImage: Redhat PFSecure W2k12r2 w2k8r2 centos ubuntu chef SUSE (default is pfsense)
+The Public IP Address of the VM will be shown when the script completes and can be used access the server.
 
-Note, these scripts require Azure Powershell 1.0.
+.EXAMPLES
+Deployment runtime positional parameters examples:
+.\Azure_infra_arm_automater.ps1myserver RedHat myresgroup myvnet -NewVNET True -NSGEnabled True
+.\Azure_infra_arm_automater.ps1myserver2 RedHat myresgroup myvnet
+.\Azure_infra_arm_automater.ps1myserver3 Suse myresgroup myvnet
+.\Azure_infra_arm_automater.ps1myserver4 w2k12 myresgroup myvnet
+.\Azure_infra_arm_automater.ps1myserver5 rserver myresgroup myvnet
+.\Azure_infra_arm_automater.ps1myserver5 sql myresgroup myvnet
+
+Runtime named parameters examples:
+Deploy SQL Server to existing VNET in existing resource group: .\Azure_infra_arm_automater.ps1-VName sqlserver1 -VMMarketImage SQL
+Deploy PFSense Server to a existing VNET in existing resource group: .\Azure_infra_arm_automater.ps1-VName pfserver1 -VMMarketImage Pfsense
+Deploy PFSense Server to a new VNET in new resource group with a Network Security Group attached: .\Azure_infra_arm_automater.ps1-VName pfserver1 -VMMarketImage Pfsense -NewVNET True -VNETName NewVNET -ResourceGroupName INFRA_RG -NSGEnabled True 
+Deploy Windows 2012 R2 Server to a new VNET in new resource group: .\Azure_infra_arm_automater.ps1-VName winserver1 -VMMarketImage w2k12r2 
