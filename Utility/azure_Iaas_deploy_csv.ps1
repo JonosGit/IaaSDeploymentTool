d1a4d9637341967d5f825d2214bc473b4b983181
John Lewis <sassy.seattle.engineer@gmail.com>
2016-08-05 09:04:52 -0700

    Added CSV Import Example and script

---
+++ Utility/azure_Iaas_deploy_csv.ps1
@@ -0,0 +1,52 @@
+﻿<#
+.SYNOPSIS
+This script supports the IaaS Deployment Script by providing the ability to deploy multiple servers in Azure using a CSV.
+
+.DESCRIPTION
+Requires the IaaS Deployment Script, provides CSV Import interop for deployment.
+
+.PARAMETER csvin
+
+.PARAMETER proscript
+
+.EXAMPLE
+\.azdeploy-csv.ps1 -proscript "C:\Temp\azdeploy.ps1" -csvin "C:\Temp\newinfra.csv"
+.NOTES
+
+.LINK
+https://github.com/JonosGit/IaaSDeploymentTool
+#>
+
+[CmdletBinding()]
+Param(
+ [Parameter(Mandatory=$True,ValueFromPipelinebyPropertyName=$true,Position=1)]
+ [string]
+ $csvin = ".\newinfra.csv",
+ [string]
+ [Parameter(Mandatory=$True,ValueFromPipelinebyPropertyName=$true,Position=0)]
+ $procscript = ".\azdeploy.ps1"
+)
+# Global
+$ErrorActionPreference = "SilentlyContinue"
+$date = Get-Date -UFormat "%Y-%m-%d-%H-%M"
+$workfolder = Split-Path $script:MyInvocation.MyCommand.Path
+
+Function DeployCSV {
+try {
+$GetPath = test-path -Path $procscript
+Write-Host $GetPath "File Exists"
+$GetPath = test-path -Path $csvin
+Write-Host $GetPath "File Exists"
+}
+catch {
+	Write-Host -foregroundcolor Yellow `
+	"$($_.Exception.Message)"; `
+	break
+}
+	finally {
+import-csv -Path $csvin -Delimiter ',' | ForEach-Object{.\azdeploy.ps1 -VMName $_.VMName -vmMarketImage $_.VMMarketImage -ResourceGroupName $_.ResourceGroupName -vNetResourceGroupName $_.vNetResourceGroupName -VNetName $_.VNetName -ConfigIPs $_.ConfigIPs -DepSub1 $_.DepSub1 -availabilityset $_.AvailabilitySet }
+	}
+}
+
+Add-AzureRmAccount
+DeployCSV
